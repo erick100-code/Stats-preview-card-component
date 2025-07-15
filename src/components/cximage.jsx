cc13726e018edb9ee3c9a3877a82dbf91cb0d3d4
Erick Eduardo Souza Araujo <ericksouza23135@gmail.com>
2025-07-15 11:35:49 -0300

    pasta images realocadas

--- src/components/cximage.jsx
+++ src/components/cximage.jsx
@@ -1,7 +1,7 @@
 function Cximage() {
     return (
         <div className="rounded-[5px] max-h-full w-[50%] relative max-[1073px]:w-full max-[1073px]:h-[30%]">
-            <img src="images/tt.jpg" alt="imagem principal"  className="rounded-[5px] h-full w-full object-cover"/> 
+            <img src="/images/tt.jpg" alt="imagem principal"  className="rounded-[5px] h-full w-full object-cover"/> 
             
             <div className="bg-[#5a27729a] absolute top-0 left-0 w-full h-full"></div>
         </div>
